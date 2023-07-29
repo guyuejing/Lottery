18e53e5edc764b16e6f580a732164554ef4fa38e
guyuejing <898179523@qq.com>
2023-07-29 21:46:25 +0800

    2023-07-29 xxl-job定时对活动状态进行扫描

--- lottery-domain/src/main/java/lottery/domain/activity/service/deploy/impl/ActivityDeployImpl.java
+++ lottery-domain/src/main/java/lottery/domain/activity/service/deploy/impl/ActivityDeployImpl.java
@@ -56,4 +56,9 @@ public class ActivityDeployImpl implements IActivityDeploy {
     public void updateActivity(ActivityConfigReq req) {
 
     }
+
+    @Override
+    public List<ActivityVO> scanToDoActivityList(Long id) {
+        return activityRepository.scanToDoActivityList(id);
+    }
 }
